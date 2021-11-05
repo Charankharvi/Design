d88d6fb28fd6c2ed2d7d756566465ce91cbcba23
Charankharvi <85237130+Charankharvi@users.noreply.github.com>
2021-11-05 13:12:15 +0530

    Add files via upload

---
+++ desgn/javascript.js
@@ -0,0 +1,37 @@
+function heartEvent() {
+    let x = document.getElementById("heartFill");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none"
+    }
+}
+
+
+
+function heartEvent1() {
+    let x = document.getElementById("heartFill1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none"
+    }
+}
+
+function heartEvent2() {
+    let x = document.getElementById("heartFill2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none"
+    }
+}
+
+function heartEvent3() {
+    let x = document.getElementById("heartFill3");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none"
+    }
+}
